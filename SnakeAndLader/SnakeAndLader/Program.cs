718d214ecffcde7b1ae7694f2da51c07fbaec92e
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-12 09:02:41 +0530

    [Sanju] ADD : Roll the die to play.

--- SnakeAndLader/SnakeAndLader/Program.cs
+++ SnakeAndLader/SnakeAndLader/Program.cs
@@ -4,10 +4,21 @@ namespace SnakeAndLader
 {
     class Program
     {
+        //Variable declaration
+        public static int currentPosition = 0;
+        //constants declaration
         const int START_POISTION = 0;
         const int END_POSTION = 100;
+
+        public static int diceRoll()
+        {
+            Random random = new Random();
+            int num = random.Next(1, 7);
+            return num;
+        }
         static void Main(string[] args)
         {
+            currentPosition += diceRoll();
             Console.WriteLine("**********Welcome to Snake And Ladder Game**************");
             Console.WriteLine("start postion is: " + START_POISTION);
             Console.WriteLine("end postion is: " + END_POSTION);
