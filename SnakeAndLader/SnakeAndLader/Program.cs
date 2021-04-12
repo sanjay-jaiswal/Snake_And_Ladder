8a77b52b04c438706b8502a9cc8773530f249dfc
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-12 08:53:52 +0530

    [Sanju] ADD : Start with single plyer at position 0.

--- SnakeAndLader/SnakeAndLader/Program.cs
+++ SnakeAndLader/SnakeAndLader/Program.cs
@@ -4,9 +4,14 @@ namespace SnakeAndLader
 {
     class Program
     {
+        const int START_POISTION = 0;
+        const int END_POSTION = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("**********Welcome to Snake And Ladder Game**************");
+            Console.WriteLine("start postion is: " + START_POISTION);
+            Console.WriteLine("end postion is: " + END_POSTION);
+
         }
     }
 }
