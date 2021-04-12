aa8023d5a7030028979a53305f4b8a00758f40c2
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-12 08:34:51 +0530

    [Sanju] ADD : Welcome message

---
+++ SnakeAndLader/SnakeAndLader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********Welcome to Snake And Ladder Game**************");
+        }
+    }
+}
