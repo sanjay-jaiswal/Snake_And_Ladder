7fa7694b2304b7c03f9efb34ec542daa7da42015
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-13 19:22:11 +0530

    [Sanju] ADD : Check for play,noPlay or ladder.

--- SnakeAndLader/SnakeAndLader/Program.cs
+++ SnakeAndLader/SnakeAndLader/Program.cs
@@ -2,27 +2,57 @@
 
 namespace SnakeAndLader
 {
-    class Program
-    {
-        //Variable declaration
-        public static int currentPosition = 0;
-        //constants declaration
-        const int START_POISTION = 0;
-        const int END_POSTION = 100;
+	class Program
+	{
+		//Variable declaration
+		public static int currentPosition = 0;
+		static Boolean playerTurn = true;
 
-        public static int diceRoll()
-        {
-            Random random = new Random();
-            int num = random.Next(1, 7);
-            return num;
-        }
-        static void Main(string[] args)
-        {
-            currentPosition += diceRoll();
-            Console.WriteLine("**********Welcome to Snake And Ladder Game**************");
-            Console.WriteLine("start postion is: " + START_POISTION);
-            Console.WriteLine("end postion is: " + END_POSTION);
+		public static int dieRollForPlay(int position, String Player)
+		{
+			//constants
+			const int NO_PLAY = 0;
+			const int LADDER = 1;
+			const int SNAKE = 2;
 
-        }
-    }
+			Random random = new Random();
+			int diceNumber = random.Next(1, 7);
+			int playOption = random.Next(0, 3);
+			switch (playOption)
+			{
+
+				case NO_PLAY:
+					break;
+				case LADDER:
+					position = position + diceNumber;
+					if (position > 100)
+					{
+						position = position - diceNumber;
+					}
+					if (position != 100)
+					{
+						dieRollForPlay(position, Player);
+
+					}
+					break;
+				case SNAKE:
+					position = position - diceNumber;
+					if (position < 0)
+					{
+						position = 0;
+					}
+					break;
+			}
+			Console.WriteLine("Position of " + Player + "  " + position);
+			return position;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("**********Welcome to Snake And Ladder Game**************");
+			int positionOfPlayer_1 = 0;
+			int positionOfPlayer_2 = 0;
+			int diceRollTotal = 0;
+		}
+	}
 }
